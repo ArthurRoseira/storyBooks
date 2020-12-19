08b69ec3d292e9d891f1da36ca37208bb8db4e8c
ArthurRoseira <arthurroseira95@gmail.com>
2020-12-19 11:49:53 -0300

    Error Templates added

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express= require('express');
 const router  = express.Router();
 const {ensureAuth, ensureGuest}= require('../middleware/auth')
+const Story = require('../models/Story')
 
 router.get('/', ensureGuest,(req,res)=>{
  res.render('login',{
@@ -8,11 +9,19 @@ router.get('/', ensureGuest,(req,res)=>{
  });
 })
 
-router.get('/dashboard',ensureAuth,(req,res)=>{
+router.get('/dashboard',ensureAuth, async (req,res)=>{
  //passing a variable to HTML with routes
- res.render('dashboard',{
+ try{
+  // .lean() transforms  Mongo document in Js object
+  const stories = await Story.find({user: req.use}).lean();
+  res.render('dashboard',{
   name: req.user.firstName,
- });
+  stories
+  });
+ }catch(err){
+  console.log(err);
+ }
+ 
 })
 
 
