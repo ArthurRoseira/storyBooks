75fb84d3640a4f53d07b8e96c267404a9897e539
ArthurRoseira <arthurroseira95@gmail.com>
2021-01-01 23:29:30 -0300

    search_box route done

--- routes/stories.js
+++ routes/stories.js
@@ -1,39 +1,43 @@
-const express= require('express');
-const router  = express.Router();
-const {ensureAuth}= require('../middleware/auth')
+const express = require('express');
+const router = express.Router();
+const { ensureAuth } = require('../middleware/auth')
 const Story = require('../models/Story')
 
 //Show add page
 //get to /stories/add
-router.get('/add', ensureAuth,(req,res)=>{
- res.render('stories/add')
+router.get('/add', ensureAuth, (req, res) => {
+  res.render('stories/add')
 })
 
-router.post('/', ensureAuth, async (req,res) =>{
- try{
-  //req.body will return the data from the submit action in add form
-  //need middleware see app.js
-  req.body.user = req.user.id //added user to storie object
-  await Story.create(req.body)
-  res.redirect('/dashboard')
- }catch(err){
-  console.log(err);
-  res.render('error/500')
- }
+router.get('/search', ensureAuth, (req, res) => {
+  res.render('stories/search')
+})
+
+router.post('/', ensureAuth, async (req, res) => {
+  try {
+    //req.body will return the data from the submit action in add form
+    //need middleware see app.js
+    req.body.user = req.user.id //added user to storie object
+    await Story.create(req.body)
+    res.redirect('/dashboard')
+  } catch (err) {
+    console.log(err);
+    res.render('error/500')
+  }
 })
 
 //Show all Stories
-router.get('/', ensureAuth, async (req,res)=>{
- try{
-  // populate let you reeference documents in other collections like a lookup
-  const stories = await Story.find({status: 'public'}).populate('user').sort({createdAt: 'desc'}).lean()
-  res.render('stories/index', {
-   stories,
-  })
- }catch(err){
-  console.log(err);
-  res.render('error/500')
- }
+router.get('/', ensureAuth, async (req, res) => {
+  try {
+    // populate let you reeference documents in other collections like a lookup
+    const stories = await Story.find({ status: 'public' }).populate('user').sort({ createdAt: 'desc' }).lean()
+    res.render('stories/index', {
+      stories,
+    })
+  } catch (err) {
+    console.log(err);
+    res.render('error/500')
+  }
 })
 
 //Show Edit Page --> route: stories/edit/:id
@@ -152,5 +156,7 @@ router.get('/user/:userId', ensureAuth, async (req, res) => {
     res.render('error/500')
   }
 })
+
+
 
 module.exports = router
